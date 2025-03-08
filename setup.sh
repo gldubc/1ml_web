1774617fcf45df068b189594a392f1462f895ceb
Guillaume Duboc <guilduboc@gmail.com>
2025-03-08 01:13:06 +0100

    Auto-update from setup-auto.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-set -e  # Exit on error
+set -e # Exit on error
+
+# Update elixir-modules from remote
+cd elixir-modules
+git fetch origin
+git merge origin/master
+cd ..
 
 # Install js_of_ocaml if not already installed
-if ! command -v js_of_ocaml &> /dev/null; then
-    opam install -y js_of_ocaml js_of_ocaml-ppx
+if ! command -v js_of_ocaml &>/dev/null; then
+  opam install -y js_of_ocaml js_of_ocaml-ppx
 fi
 
 # Create docs directory structure
@@ -14,3 +20,4 @@ dune build
 # Ensure target is writable if it exists
 [ -f docs/js/web.js ] && chmod u+w docs/js/web.js
 cp _build/default/web.bc.js docs/js/web.js
+
